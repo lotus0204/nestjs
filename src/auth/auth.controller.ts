a43123fc7b92160a454d26deb27b7263e7d07460
lotus0204 <lotusrb0204@gmail.com>
2021-11-04 11:01:50 +0900

    Add:authcontrolr

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post, Put, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Put, UseGuards, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +17,12 @@ export class AuthController {
   @Put('/signin')
   signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<{accessToken:string}>{
     return this.authService.signIn(authCredentialsDto);
+  }
+
+  //이부분 전반적인 흐름이 잘 이해가 안된다.
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@GetUser() user: User) {
+    console.log('user', user);
   }
 }
