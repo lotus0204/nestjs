f2dbd8e5b8ed3e08535ba46d89416e9b9696aff3
lotus0204 <lotusrb0204@gmail.com>
2021-11-16 22:47:59 +0900

    공부 및 주석정리

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -12,9 +12,7 @@ import { User } from 'src/auth/user.entity';
 export class BoardsController {
   constructor(private boardsService: BoardsService) { }
   
-  // @Get()//'/'루트야
-  // //타입을 정의해 주는 것은 좋아. 보는 사람도 좋고 에러의 부분에서도 쉽게 확인할 수 있어
-  // //핸들러라고 불러 
+  // @Get()//'/'루트
   @Get()
   getAllBoard(): Promise<Board[]> {
     return this.boardsService.getAllBoards();
@@ -44,27 +42,4 @@ export class BoardsController {
   ):Promise<Board> {
     return this.boardsService.updateBoardStatus(id, status);
   }
-  // @Get('/:id')
-  // getBoardById(
-  //   @Param('id') id: string
-  // ): Board {
-  //   const found = this.boardsService.getBoardById(id);
-  //   if (!found) throw new NotFoundException();
-  //   return found;
-
-  // }
-
-  // @Delete('/:id')
-  // deleteBoard(@Param('id') id: string): void{
-  //   this.boardsService.deleteBoard(id);
-  // }
-
-  // @Patch('/:id/status')
-  // updateBoardStatus(
-  //   @Param('id') id: string,
-  //   @Body('status') status: BoardStatus,
-  // ): Board {
-  //   return this.boardsService.updateBoardStatus(id,status)
-  // }
-    
 }
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -7,6 +7,7 @@ import { BoardsService } from './boards.service';
 
 @Module({
   imports: [//다른 곳에서도 보드레파지토리를 쓸 수 있도록... 그럼 익스포트 아닌가? 잘 이해안됨
+    //이것들을 임포트 해와서 여기서 쓴다는 이야기임!!!
     TypeOrmModule.forFeature([BoardRepository]),
     AuthModule
   ],
